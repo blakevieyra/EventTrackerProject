9e8b081bfb6955401d3d6156ebbf6375420f48ae
Blake Vieyra <blake.vieyra@gmail.com>
2024-02-23 23:23:40 -0800

    feature complete

--- RESTArtistsTracker/src/main/resources/static/js/script.js
+++ RESTArtistsTracker/src/main/resources/static/js/script.js
@@ -1,41 +1,40 @@
 window.addEventListener("load", function(e) {
-	console.log(e);
 	e.preventDefault();
-	init()
+	init();
 });
 
 function init() {
 	var audioSources = ['music/06 Ramble On.m4a',
-	 'music/01 Hells Bells.m4a',
-	 'music/02 Meant to Live.m4a',
-	 'music/13 Titanium (feat. Sia).m4a',
-	 'music/1-06 Wish You Were Here.m4a',
-	 'music/05 Shes Everything.m4a',
-	 'music/04 Electric Feel.m4a',
-	 'music/02 No One Like You.m4a',
-	 'music/03 If You Could Only See.m4a',
-	 'music/01 The A Team.m4a',
-	 'music/02 Dont Take the Girl.m4a',
-	 'music/02 Carrying Your Love With Me.m4a'];
-    var currentSourceIndex = 0; 
+		'music/01 Hells Bells.m4a',
+		'music/02 Meant to Live.m4a',
+		'music/13 Titanium (feat. Sia).m4a',
+		'music/1-06 Wish You Were Here.m4a',
+		'music/05 Shes Everything.m4a',
+		'music/04 Electric Feel.m4a',
+		'music/02 No One Like You.m4a',
+		'music/03 If You Could Only See.m4a',
+		'music/01 The A Team.m4a',
+		'music/02 Dont Take the Girl.m4a',
+		'music/02 Carrying Your Love With Me.m4a'];
+	var index = 0;
 
-    var audio = document.createElement('audio');
-    audio.src = audioSources[currentSourceIndex]; 
-    audio.controls = true; 
-    audio.volume = 0.3;
+	var audio = document.createElement('audio');
+	audio.src = audioSources[index];
+	audio.controls = true;
+	audio.volume = 0.3;
 
-    document.body.appendChild(audio);
+	document.body.appendChild(audio);
 
-    document.getElementById('playButton').addEventListener('click', function() {
-        if (audio.paused) {
-            audio.play();
-        } else {
-            audio.pause();
-            currentSourceIndex = (currentSourceIndex + 1) % audioSources.length;
-            audio.src = audioSources[currentSourceIndex];
-            audio.play();
-        }
-    });
+	document.getElementById('playButton').addEventListener('click', function() {
+		if (audio.paused) {
+			audio.play();
+		} else {
+			audio.pause();
+			index = (index + 1) % audioSources.length;
+			audio.src = audioSources[index];
+			audio.play();
+		}
+	});
 	document.addingArtistForm.addArtistbtn.addEventListener('click', function(e) {
 		e.preventDefault();
 		console.log('adding artists');
@@ -47,7 +46,6 @@ function init() {
 		addArtist(artist);
 	});
 	loadAllArtists();
-
 };
 
 function loadAllArtists() {
@@ -73,21 +71,17 @@ function displayArtists(artists) {
 	if (artists && Array.isArray(artists) && artists.length > 0) {
 		let tbody = document.getElementById('artistsTable');
 		tbody.textContent = '';
-		
+
+
 		for (let artist of artists) {
 			let tr = document.createElement('tr');
 			tbody.appendChild(tr);
-
 			let td = document.createElement('td');
 			let img = document.createElement('img');
 			img.classList.add('thumbnail-image');
 			img.src = artist.image;
 			img.alt = "Artist Image";
 			td.appendChild(img);
-			tr.appendChild(td);
-
-			td = document.createElement('td');
-			td.textContent = artist.id;
 			tr.appendChild(td);
 
 			td = document.createElement('td');
@@ -132,13 +126,13 @@ function deleteArtist(artist) {
 	xhr.send();
 }
 
-function deleteSong(artist, song) {
+function deleteSong(song) {
 	let xhr = new XMLHttpRequest();
-	xhr.open('DELETE', 'api/artists/' + artist.id + "/songs/" + song.id);
+	xhr.open('DELETE', 'api/artists/' + document.targe + "/songs/" + song.id);
 	xhr.onreadystatechange = function() {
 		if (xhr.readyState === XMLHttpRequest.DONE) {
 			if (xhr.status === 204) {
-				loadAllArtists();
+				getSongs(artist)
 			} else {
 				let doc = document.getElementById('addArtistsDiv');
 				let error = document.createElement('h1');
@@ -156,8 +150,7 @@ function addArtist(artist) {
 	xhr.onreadystatechange = function() {
 		if (xhr.readyState === XMLHttpRequest.DONE) {
 			if (xhr.status === 201) {
-				let artist = JSON.parse(xhr.responseText);
-				showArtistInfo(artist);
+				loadAllArtists();
 			} else {
 				let doc = document.getElementById('addArtistsDiv');
 				let error = document.createElement('h1');
@@ -171,6 +164,26 @@ function addArtist(artist) {
 	xhr.send(newArtistJSON);
 }
 
+function addSong(artist, song) {
+	let xhr = new XMLHttpRequest();
+	xhr.open('POST', 'api/artists/' + artist.id + "/songs");
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState === XMLHttpRequest.DONE) {
+			if (xhr.status === 201) {
+				getSongs(artist);
+			} else {
+				let doc = document.getElementById('addArtistsDiv');
+				let error = document.createElement('h1');
+				error.textContent = "Artist Not Created: " + xhr.status;
+				doc.appendChild(error);
+			}
+		}
+	};
+	xhr.setRequestHeader("Content-type", "application/json");
+	let newSongJSON = JSON.stringify(song);
+	xhr.send(newSongJSON);
+}
+
 function getSongs(artist) {
 	let xhr = new XMLHttpRequest();
 	xhr.open('GET', 'api/artists/' + artist.id + "/songs");
@@ -178,11 +191,12 @@ function getSongs(artist) {
 		if (xhr.readyState === XMLHttpRequest.DONE) {
 			if (xhr.status === 200) {
 				let songs = JSON.parse(xhr.responseText);
+				console.log(songs);
 				displaySongs(artist, songs);
 			} else {
-				let doc = document.getElementById('artistData');
+				let doc = document.getElementById('songData');
 				let error = document.createElement('h1');
-				error.textContent = "No artist found " + xhr.status; 
+				error.textContent = "No songs found " + xhr.status;
 				doc.appendChild(error);
 			}
 		}
@@ -190,64 +204,63 @@ function getSongs(artist) {
 	xhr.send();
 }
 
-function displayArtistAndSongs(artist, songs) {
+function displaySongs(artist, songs) {
 	if (songs && Array.isArray(songs) && songs.length > 0) {
-		let artistDataDiv = document.getElementById('artistData');
-		artistDataDiv.textContent = '';
-	
-		let artistDetailsDiv = document.getElementById('artistDetails');
-		artistDetailsDiv.textContent = '';
-		
-		let img = document.createElement('img');
-		img.classList.add('display-image');
-		img.src = artist.image;
-		img.alt = "Artist Image";
-		artistDetailsDiv.appendChild(img);
 
-		let h3 = document.createElement('h4');
-		h3.textContent = artist.name;
-		artistDetailsDiv.appendChild(h3);
+		let tbody = document.getElementById('songTable');
+		tbody.textContent = '';
 
-		let h5 = document.createElement('h5');
-		h5.textContent = artist.band;
-		artistDetailsDiv.appendChild(h5);
-	
-		let ul = document.createElement("ul");
 		for (let song of songs) {
-			
-			let li = document.createElement('li');
-			li.textContent = song.id;
-			ul.appendChild(li);
-			li = document.createElement('li');
-			li.textContent = song.title;
-			ul.appendChild(li);
-			li = document.createElement('li');
-			li.textContent = song.album;
-			ul.appendChild(li);
-			li = document.createElement('li');
-			li.textContent = song.length;
-			ul.appendChild(li);
-			
-			li = document.createElement('li');
+			tr = document.createElement('tr');
+			let td = document.createElement('td');
+			td.textContent = song.name;
+			tr.appendChild(td);
+			td = document.createElement('td');
+			td.textContent = song.genre;
+			tr.appendChild(td);
+			td = document.createElement('td');
+			td.textContent = song.album;
+			tr.appendChild(td);
+			td = document.createElement('td');
+			td.textContent = song.length;
+			tr.appendChild(td);
+			td = document.createElement('td');
 			let delBtn = document.createElement("button")
 			delBtn.textContent = "Delete";
 			delBtn.classList.add('btn');
 			delBtn.addEventListener("click", function(e) {
 				deleteSong(artist, song);
 			});
-			li.appendChild(delBtn);
-			ul.appendChild(li);
+			td.appendChild(delBtn);
+			tbody.appendChild(tr);
 		}
-		artistDetailsDiv.appendChild(ul);
 		let returnButton = document.createElement('button');
-			returnButton.textContent = "Go Back";
-			returnButton.classList.add('btn');
-			returnButton.addEventListener('click', function(evt) {
-				loadAllArtists();
-			});
-			artistDetailsDiv.appendChild(returnButton);
+		returnButton.textContent = "Go Back";
+		returnButton.classList.add('btn');
+		returnButton.addEventListener('click', function(evt) {
+			loadAllArtists();
+		});
+		tbody.appendChild(returnButton);
+		document.addingSongForm.addSongbtn.addEventListener('click', function(e) {
+			e.preventDefault();
+			console.log('adding songs');
+			let song = {
+				name: addingSongForm.name.value,
+				genre: addingSongForm.genre.value,
+				album: addingSongForm.album.value,
+				length: addingSongForm.length.value,
+			};
+			addSong(artist, song);
+		});
+
 	}
 }
+//song: {
+//name: addingArtistForm.name.value
+//name: addingArtistForm.name.value
+//album: addingArtistForm.album.value
+//length: addingArtistForm.length.value
+//}
 
 
 
